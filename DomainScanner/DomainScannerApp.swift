6708dc468f142dbbc00198f7f4b774aec5221707
KreatixDev <hamalainen.miska@outlook.com>
2026-08-21 17:21:48 +0300

    Set up notification authorization on startup
    Initialize notification manager and request authorization on app launch.

--- DomainScanner/DomainScannerApp.swift
+++ DomainScanner/DomainScannerApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import UserNotifications
 
 @main
 struct DomainScannerApp: App {
+    init() {
+        // Alustetaan ilmoituskeskus ja pyydetään käyttöoikeus käynnistyksessä
+        NotificationManager.shared.requestAuthorization()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
